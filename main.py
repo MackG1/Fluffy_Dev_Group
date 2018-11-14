c55a813f96fdd045f61d9626db074ef5407277d3
MackG1 <mgunt2@ocdsb.ca>
2018-11-13 22:18:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-main run code
+import pygame
+
+pygame.init()
+screen_width = 1000
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+sprites_list = pygame.sprite.Group()
+
+carryOn = True
+
+cat = Attacker(50, 50)
+sprites_list.add(cat)
+
+while carryOn:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            carryOn = False
+
+    sprites_list.draw(screen)
+    pygame.display.flip()
+    clock.tick(60)
+
