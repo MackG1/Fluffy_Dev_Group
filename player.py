029ea3fd57b1a8e5946cb65fed35442f4010ac5a
mac <mgunt2@ocdsb.ca>
2018-11-13 22:24:36 -0500

    This files all work, this is basic layout

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
 import pygame
 
 
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, x_position, y_position):
+        super().__init__()
         self.x_position = x_position
         self.y_position = y_position
 
