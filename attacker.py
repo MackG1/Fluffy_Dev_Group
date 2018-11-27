cf9de40e582c9cf5a72eb1de51d81573aac72372
mac <mgunt2@ocdsb.ca>
2018-11-27 08:13:40 -0500

    This files all work,Adding comments

--- attacker.py
+++ attacker.py
@@ -25,21 +25,23 @@ class Attacker(pygame.sprite.Sprite):
         self.last_move_y = 0
 
     def move(self, player_x, player_y):
+        # Check if alive, attacker doesn't move if not alive
         if self.alive:
+            # x difference and direction
             delta_x = player_x - self.rect.x
-
             if delta_x <= 0:
                 x_direction = -1
             else:
                 x_direction = 1
 
+            # y difference and direction
             delta_y = player_y - self.rect.y
-
             if delta_y <= 0:
                 y_direction = -1
             else:
                 y_direction = 1
 
+            # compare absolute value of delta x and delta y
             if abs(delta_x) > abs(delta_y):
                 x_movement = 2
                 y_movement = 0
